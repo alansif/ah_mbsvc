5fd5a3f1e8e5f2254813ede9e6eafacc18eef1dd
wg <alansif@sina.com>
2018-05-12 23:51:38 +0800

    create card ok

--- index.js
+++ index.js
@@ -106,13 +106,17 @@ app.post('/api/v1/card', function(req, res) {
 		res.status(400).json({status:{code:1001,message:'证件号码格式错误'}});
 		return;
 	}
+    let f_name = trim(req.body.name || '');
+    if (f_name.length === 0) {
+        res.status(400).json({status:{code:1002,message:'需要姓名'}});
+        return;
+    }
 	let f_price = parseFloat(req.body.price);
 	if (isNaN(f_price)) {
-		res.status(400).json({status:{code:1002,message:'价格填写错误'}});
+		res.status(400).json({status:{code:1003,message:'价格填写错误'}});
 		return;
 	}
 	id = trim(id).toUpperCase();
-	let f_name = trim(req.body.name || '');
 	let f_sex = trim(req.body.sex || '');
 	let f_mobile = trim(req.body.mobile || '');
 	let f_address = trim(req.body.address || '');
@@ -141,8 +145,16 @@ app.post('/api/v1/card', function(req, res) {
 			maxcid = parseInt(maxcid) + 1;
 			maxcid = '' + maxcid;
 			console.log({maxuid, maxcid});
-			let s1 = "INSERT INTO Tr_member_CardStatus(UserID,卡号,卡状态,描述,操作人员,操作日期,开卡门店) ";
-			s1 += "VALUES(@uid,@cid,1,'已发卡投入使用',@operator,GETDATE(),'总部')";
+			const s1 = "INSERT INTO Tr_member_CardStatus(UserID,卡号,卡状态,描述,操作人员,操作日期,开卡门店) " +
+					"VALUES(@uid,@cid,1,'已发卡投入使用',@operator,GETDATE(),'总部')";
+			const s2 = "INSERT INTO Tr_member_Cardbaseinfo(UserID,卡号,姓名,性别,身份证号码,联系电话,通讯地址,会员期限类别,益生套餐,采购健老,首次采购价格," +
+					"有效期起始,有效期截止,签发日期,用户密码,健康顾问,发卡门店,会员状态,操作日期,操作人员,卡片开启,备注,使用分类,定制电话) " +
+					"VALUES(@uid,@cid,@username,@sex,@idnum,@mobile,@address,'3',3,0,@price,@period0,@period1,GETDATE(),'888888',@advisor,'总部'," +
+					"'新卡使用',GETDATE(),@operator,'开启',@comment,'新会员',@altphone)";
+			const s3 = "INSERT INTO Tr_member_Moneydetail(UserID,卡号,收款类型,项目名称,收款金额,收款门店,收款日期,收款人员,姓名,性别,身份证号码) " +
+					"VALUES(@uid,@cid,'首次入会','益生套餐',@price,'总部',GETDATE(),@operator,@username,@sex,@idnum)";
+			const s4 = "INSERT INTO Tr_member_RegCardinfo(UserID,卡号,姓名,性别,身份证号码,联系电话,通讯地址,健康顾问,发卡门店,发卡日期,操作人员) " +
+					"VALUES(@uid,@cid,@username,@sex,@idnum,@mobile,@address,@advisor,'总部',GETDATE(),@operator)";
 			const trans = pool80.transaction();
 			trans.begin(err => {
 				if (err) {
@@ -154,28 +166,42 @@ app.post('/api/v1/card', function(req, res) {
 				trans.on('rollback', aborted => {
 					rolledBack = true;
 				});
-				trans.request().input('uid',maxuid).input('cid',maxcid).input('operator',f_operator).query(s1, (err, result) => {
-						if (err) {
-							if (!rolledBack) {
-								trans.rollback(err_rb => {
-									if (err_cb) {
-										console.error('rollback failed');
-									}
-								});
-							}
-						} else {
-							trans.commit(err_cm => {
-								if (err_cm) {
-									console.error('commit failed');
-								}
-							});
-						}
-					});
+				(async function(){
+					try {
+                        await trans.request().input('uid',maxuid).input('cid',maxcid).input('operator',f_operator).query(s1);
+                        await trans.request().input('uid',maxuid).input('cid',maxcid).input('username',f_name).input('sex',f_sex)
+							.input('idnum',id).input('mobile',f_mobile).input('address',f_address).input('price',f_price)
+							.input('period0',f_period0).input('period1',f_period1).input('advisor',f_advisor)
+							.input('operator',f_operator).input('comment',f_comment).input('altphone',f_altphone).query(s2);
+                        await trans.request().input('uid',maxuid).input('cid',maxcid).input('price',f_price * 3.0)
+							.input('operator',f_operator).input('username',f_name).input('sex',f_sex).input('idnum',id).query(s3);
+                        await trans.request().input('uid',maxuid).input('cid',maxcid).input('username',f_name).input('sex',f_sex)
+                            .input('idnum',id).input('mobile',f_mobile).input('address',f_address).input('advisor',f_advisor)
+                            .input('operator',f_operator).query(s4);
+                        trans.commit(err_cm => {
+                            if (err_cm) {
+                                console.error('commit failed');
+                                res.status(500).end();
+                                return;
+                            }
+                            res.status(200).json({status:{code:0,message:'开卡成功'},data:{uid:maxuid,cid:maxcid}});
+						});
+					} catch(err) {
+                        if (!rolledBack) {
+                            rolledBack = true;
+                            trans.rollback(err_rb => {
+                                if (err_rb) {
+                                    console.error('rollback failed');
+                                }
+                            });
+                        }
+                        res.status(500).end();
+					}
+				})();
 			});
-			res.status(200).json({status:{code:0,message:'ok'}});
 		} catch(err) {
 			console.error(err);
-            		res.status(500).end();
+			res.status(500).end();
 		}
 	};
 	f();
