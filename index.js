4baad8dded2defa3af02fdb05870a387e8dce35c
wg <alansif@sina.com>
2018-05-21 14:46:40 +0800

    add email

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post('/api/v1/login', function(req, res){
 	f();
 });
 
-let sql_guestinfo = "select Name, Sex, Mobile, Address from T_Guest_Info where PaperValue=@id";
+let sql_guestinfo = "select Name, Sex, Mobile, Address, Email from T_Guest_Info where PaperValue=@id";
 
 app.get('/api/v1/guest/:id/info', function(req, res){
     const id = req.params['id'];
@@ -121,6 +121,80 @@ app.get('/api/v1/card', function(req, res) {
 	})();
 });
 
+app.post('/api/v1/card/:id/changeperiod', function(req, res) {
+    let id = req.params['id'];
+    if (id.length !== 18) {
+        res.status(400).json({status: {code: 1001, message: '证件号码格式错误'}});
+        return;
+    }
+    let f_period0 = trim(req.body.period0 || '');
+    let f_period1 = trim(req.body.period1 || '');
+    let f_comment = trim(req.body.comment || '');
+    let f_operator = trim(req.body.operator || '');
+    (async () => {
+		try {
+            result = await pool80.request().input('idnum', id)
+                .query("select * from Tr_member_Cardbaseinfo where 身份证号码=@idnum AND 会员状态<>'已经停用'");
+            if (result.recordset.length === 0) {
+                res.status(400).json({status:{code:1005,message:'不是会员或会员卡已停用'}});
+                return;
+            }
+            const r = result.recordset[0];
+            const f_userid = r['UserID'];
+            const f_cardid = r['卡号'];
+            const f_name = r['姓名'];
+            const f_sex = r['性别'];
+            const f_oldp0 = r['有效期起始'];
+            const f_oldp1 = r['有效期截止'];
+            const s1 = "UPDATE Tr_member_Cardbaseinfo SET 有效期起始=@p0,有效期截止=@p1 WHERE 身份证号码=@idnum";
+            const s2 = "INSERT INTO Tr_member_ChangePeriod(UserID,卡号,姓名,性别,身份证号码,原有效期起始,原有效期截止,新有效期起始,新有效期截止,备注,操作日期,操作人员) " +
+				"VALUES(@uid,@cid,@username,@sex,@idnum,@oldp0,@oldp1,@newp0,@newp1,@comment,GETDATE(),@operator)";
+            const trans = pool80.transaction();
+            trans.begin(err => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).end();
+                    return;
+                }
+                let rolledBack = false;
+                trans.on('rollback', aborted => {
+                    rolledBack = true;
+                });
+                (async function(){
+                    await trans.request().input('idnum',id).input('p0',f_period0).input('p1',f_period1).query(s1);
+                    await trans.request().input('uid',f_userid).input('cid',f_cardid).input('username',f_name).input('sex',f_sex).input('idnum',id)
+						.input('oldp0',f_oldp0).input('oldp1',f_oldp1).input('newp0',f_period0).input('newp1',f_period1).input('comment',f_comment)
+						.input('operator',f_operator).query(s2);
+                    try {
+                        trans.commit(err_cm => {
+                            if (err_cm) {
+                                console.error('commit failed');
+                                res.status(500).end();
+                                return;
+                            }
+                            res.status(200).json({status:{code:0,message:'操作成功'},data:{}});
+                        });
+                    } catch(err) {
+                        if (!rolledBack) {
+                            rolledBack = true;
+                            trans.rollback(err_rb => {
+                                if (err_rb) {
+                                    console.error('rollback failed');
+                                }
+                            });
+                        }
+                        console.error(err);
+                        res.status(500).end();
+                    }
+                })();
+            });
+		}catch(err) {
+            console.error(err);
+            res.status(500).end();
+		}
+	})();
+});
+
 app.post('/api/v1/card/:id/refund', function(req, res) {
     let id = req.params['id'];
     if (id.length !== 18) {
@@ -133,7 +207,7 @@ app.post('/api/v1/card/:id/refund', function(req, res) {
                 result = await pool80.request().input('idnum', id)
                     .query("select * from Tr_member_Cardbaseinfo where 身份证号码=@idnum AND 会员状态<>'已经停用'");
                 if (result.recordset.length === 0) {
-                    res.status(400).json({status:{code:1005,message:'会员卡已停用'}});
+                    res.status(400).json({status:{code:1005,message:'不是会员或会员卡已停用'}});
                     return;
                 }
                 const r = result.recordset[0];
@@ -142,14 +216,14 @@ app.post('/api/v1/card/:id/refund', function(req, res) {
                 const f_name = r['姓名'];
                 const f_sex = r['性别'];
                 const f_amount = r['账户余额'];
-                const s1 = "UPDATE Tr_member_CardStatus SET 卡状态=0,旧卡号='TK0000',描述='退卡可以重新使用',操作日期=GETDATE(),操作人员=@operator "
+                const s1 = "UPDATE Tr_member_CardStatus SET 卡状态=0,旧卡号='TK0000',描述='退卡可以重新使用',操作日期=GETDATE(),操作人员=@operator,标记='Y' "
 					+ "WHERE UserID=@uid AND 卡状态=1";
-                const s2 = "UPDATE Tr_member_Cardbaseinfo SET 会员状态='已经停用',操作日期=GETDATE(),操作人员=@operator "
+                const s2 = "UPDATE Tr_member_Cardbaseinfo SET 会员状态='已经停用',操作日期=GETDATE(),操作人员=@operator,标记='Y' "
                     + "WHERE UserID=@uid AND 会员状态<>'已经停用'";
-                const s3 = "INSERT INTO Tr_member_ReturnCardinfo(UserID,卡号,姓名,性别,身份证号码,联系电话,通讯地址,健康顾问,退卡日期,操作人员,退卡金额,退卡门店) " +
-					"VALUES(@uid,@cid,@username,@sex,@idnum,@mobile,@address,@advisor,GETDATE(),@operator,@amount,'总部')";
-                const s4 = "INSERT INTO Tr_member_Moneydetail(UserID,卡号,收款门店,收款日期,收款类型,项目名称,收款金额,收款人员,姓名,性别,身份证号码) " +
-                    "VALUES(@uid,@cid,'总部',GETDATE(),'会员退卡','退卡余额',-@amount,@operator,@username,@sex,@idnum)";
+                const s3 = "INSERT INTO Tr_member_ReturnCardinfo(UserID,卡号,姓名,性别,身份证号码,联系电话,通讯地址,健康顾问,退卡日期,操作人员,退卡金额,退卡门店,标记) " +
+					"VALUES(@uid,@cid,@username,@sex,@idnum,@mobile,@address,@advisor,GETDATE(),@operator,@amount,'总部','Y')";
+                const s4 = "INSERT INTO Tr_member_Moneydetail(UserID,卡号,收款门店,收款日期,收款类型,项目名称,收款金额,收款人员,姓名,性别,身份证号码,标记) " +
+                    "VALUES(@uid,@cid,'总部',GETDATE(),'会员退卡','退卡余额',-@amount,@operator,@username,@sex,@idnum,'Y')";
                 const trans = pool80.transaction();
                 trans.begin(err => {
                     if (err) {
@@ -236,10 +310,10 @@ app.post('/api/v1/card/:id/deposit', function(req, res) {
             const f_cardid = r['卡号'];
             const f_name = r['姓名'];
             const f_sex = r['性别'];
-            const s1 = "INSERT INTO Tr_member_Moneydetail(UserID,卡号,收款门店,收款日期,收款类型,项目名称,收款金额,收款人员,姓名,性别,身份证号码,备注) " +
-					"VALUES(@uid,@cid,'总部',GETDATE(),'日常储值',@dpt,@amount,@operator,@username,@sex,@idnum,@comment)";
-            const s2 = "UPDATE Tr_member_Cardbaseinfo SET 账户预存=账户预存+@amount,账户余额=账户余额+@amount WHERE 身份证号码=@idnum AND 会员状态<>'已经停用'";
-            const s3 = "UPDATE Tr_member_Cardbaseinfo SET 赠券金额=赠券金额+@amount,赠券余额=赠券余额+@amount WHERE 身份证号码=@idnum AND 会员状态<>'已经停用'";
+            const s1 = "INSERT INTO Tr_member_Moneydetail(UserID,卡号,收款门店,收款日期,收款类型,项目名称,收款金额,收款人员,姓名,性别,身份证号码,备注,标记) " +
+					"VALUES(@uid,@cid,'总部',GETDATE(),'日常储值',@dpt,@amount,@operator,@username,@sex,@idnum,@comment,'Y')";
+            const s2 = "UPDATE Tr_member_Cardbaseinfo SET 账户预存=账户预存+@amount,账户余额=账户余额+@amount,标记='Y' WHERE 身份证号码=@idnum AND 会员状态<>'已经停用'";
+            const s3 = "UPDATE Tr_member_Cardbaseinfo SET 赠券金额=赠券金额+@amount,赠券余额=赠券余额+@amount,标记='Y' WHERE 身份证号码=@idnum AND 会员状态<>'已经停用'";
             const trans = pool80.transaction();
             trans.begin(err => {
                 if (err) {
@@ -326,18 +400,18 @@ app.post('/api/v1/card/:id/transfer', function(req, res) {
                 return;
             }
             const r1 = result1.recordset[0];
-            const s1 = "UPDATE Tr_member_Cardbaseinfo SET 会员状态='已经停用',操作日期=GETDATE(),操作人员=@operator " +
+            const s1 = "UPDATE Tr_member_Cardbaseinfo SET 会员状态='已经停用',操作日期=GETDATE(),操作人员=@operator,标记='Y' " +
 					"WHERE 身份证号码=@id0 AND 会员状态<>'已经停用'";
             const s2 = "INSERT INTO Tr_member_Cardbaseinfo(UserID,卡号,姓名,性别,身份证号码,联系电话,通讯地址,用户密码,操作日期,操作人员,会员状态," +
 					"会员期限类别,益生套餐,采购健老,首次采购价格,享受折扣,有效期起始,有效期截止,签发日期,健康顾问,发卡门店,卡片开启,备注,定制电话," +
-					"账户预存,日常消费,账户余额,已用益生套餐,已用采购健老,会员经理,赠券金额,赠券消费,赠券余额) " +
+					"账户预存,日常消费,账户余额,已用益生套餐,已用采购健老,会员经理,赠券金额,赠券消费,赠券余额,标记,电子邮件) " +
 					"VALUES(@uid,@cid,@username,@sex,@id1,@mobile,@address,@passwd,GETDATE(),@operator,'会员转卡'," +
 					"@level,@tc,@jl,@price,@discount,@period0,@period1,@issuedt,@advisor,@shop,@status,@comment,@altphone," +
-					"@z1,@z2,@z3,@z4,@z5,@z6,@z7,@z8,@z9)";
-            const s3 = "INSERT INTO Tr_member_ChangeCardinfo(UserID,卡号,原姓名,原性别,原身份证号码,转卡日期,操作人员,姓名,性别,身份证号码,用户密码,联系电话,通讯地址,转卡门店,定制电话) " +
-					"VALUES(@uid,@cid,@username0,@sex0,@id0,GETDATE(),@operator,@username,@sex,@id1,@passwd,@mobile,@address,'总部',@altphone)";
-            const s4 = "INSERT INTO Tr_member_Moneydetail(UserID,卡号,收款门店,收款日期,收款类型,项目名称,收款金额,收款人员,姓名,性别,身份证号码) " +
-					"VALUES(@uid,@cid,'总部',GETDATE(),'会员转卡','转卡费',@fee,@operator,@username,@sex,@id1)";
+					"@z1,@z2,@z3,@z4,@z5,@z6,@z7,@z8,@z9,'Y',@email)";
+            const s3 = "INSERT INTO Tr_member_ChangeCardinfo(UserID,卡号,原姓名,原性别,原身份证号码,转卡日期,操作人员,姓名,性别,身份证号码,用户密码,联系电话,通讯地址,转卡门店,定制电话,标记) " +
+					"VALUES(@uid,@cid,@username0,@sex0,@id0,GETDATE(),@operator,@username,@sex,@id1,@passwd,@mobile,@address,'总部',@altphone,'Y')";
+            const s4 = "INSERT INTO Tr_member_Moneydetail(UserID,卡号,收款门店,收款日期,收款类型,项目名称,收款金额,收款人员,姓名,性别,身份证号码,标记) " +
+					"VALUES(@uid,@cid,'总部',GETDATE(),'会员转卡','转卡费',@fee,@operator,@username,@sex,@id1,'Y')";
             const trans = pool80.transaction();
             trans.begin(err => {
                 if (err) {
@@ -359,7 +433,7 @@ app.post('/api/v1/card/:id/transfer', function(req, res) {
                             .input('issuedt',r0['签发日期']).input('advisor',r0['健康顾问']).input('shop',r0['发卡门店']).input('status',r0['卡片开启'])
                             .input('comment',r0['备注']).input('altphone',f_altphone).input('z1',r0['账户预存']).input('z2',r0['日常消费'])
                             .input('z3',r0['账户余额']).input('z4',r0['已用益生套餐']).input('z5',r0['已用采购健老']).input('z6',r0['会员经理'])
-                            .input('z7',r0['赠券金额']).input('z8',r0['赠券消费']).input('z9',r0['赠券余额']).query(s2);
+                            .input('z7',r0['赠券金额']).input('z8',r0['赠券消费']).input('z9',r0['赠券余额']).input('email',r1['Email']).query(s2);
                         await trans.request().input('uid',r0['UserID']).input('cid',r0['卡号']).input('username0',r0['姓名']).input('sex0',r0['性别'])
 							.input('id0',id0).input('operator',f_operator).input('username',r1['Name']).input('sex',r1['Sex']).input('id1',id1)
                             .input('passwd',r0['用户密码']).input('mobile',r1['Mobile']).input('address',r1['Address']).input('altphone',f_altphone)
@@ -424,20 +498,22 @@ app.post('/api/v1/card/:id/renew', function(req, res) {
                 return;
             }
             const r = result.recordset[0];
-            if (r['首次采购价格'] < f_price / 3.0) {
+/*
+			if (r['首次采购价格'] < f_price / 3.0) {
                 res.status(400).json({status:{code:1006,message:'续卡价格高于首次采购价格'}});
                 return;
 			}
+*/
             const f_userid = r['UserID'];
             const f_cardid = r['卡号'];
             const f_name = r['姓名'];
             const f_sex = r['性别'];
-            const s1 = "INSERT INTO Tr_member_CardAddYearinfo(UserID,卡号,会员期限类别,益生套餐,采购健老,有效期起始,有效期截止,续卡门店,续卡日期,操作人员,采购价格,姓名,性别,身份证号码)" +
-				"VALUES(@uid,@cid,'3',3,0,@period0,@period1,'总部',GETDATE(),@operator,@price,@username,@sex,@idnum)";
-            const s2 = "INSERT INTO Tr_member_Moneydetail(UserID,卡号,收款类型,项目名称,收款金额,收款门店,收款日期,收款人员,姓名,性别,身份证号码)" +
-                "VALUES(@uid,@cid,'会员续卡','益生套餐',@price,'总部',GETDATE(),@operator,@username,@sex,@idnum)";
+            const s1 = "INSERT INTO Tr_member_CardAddYearinfo(UserID,卡号,会员期限类别,益生套餐,采购健老,有效期起始,有效期截止,续卡门店,续卡日期,操作人员,采购价格,姓名,性别,身份证号码,标记)" +
+				"VALUES(@uid,@cid,'3',3,0,@period0,@period1,'总部',GETDATE(),@operator,@price,@username,@sex,@idnum,'Y')";
+            const s2 = "INSERT INTO Tr_member_Moneydetail(UserID,卡号,收款类型,项目名称,收款金额,收款门店,收款日期,收款人员,姓名,性别,身份证号码,标记)" +
+                "VALUES(@uid,@cid,'会员续卡','益生套餐',@price,'总部',GETDATE(),@operator,@username,@sex,@idnum,'Y')";
             const s3 = "UPDATE Tr_member_Cardbaseinfo SET 会员期限类别='3',益生套餐=益生套餐+3,使用分类='连续续卡'," +
-				"首次采购价格=@price,有效期起始=@period0,有效期截止=@period1 WHERE 身份证号码=@idnum AND 会员状态<>'已经停用'";
+				"首次采购价格=@price,有效期起始=@period0,有效期截止=@period1,标记='Y' WHERE 身份证号码=@idnum AND 会员状态<>'已经停用'";
             const trans = pool80.transaction();
             trans.begin(err => {
                 if (err) {
@@ -473,6 +549,7 @@ app.post('/api/v1/card/:id/renew', function(req, res) {
                                 }
                             });
                         }
+                        console.error(err);
                         res.status(500).end();
                     }
                 })();
@@ -504,6 +581,7 @@ app.post('/api/v1/card', function(req, res) {
 	let f_sex = trim(req.body.sex || '');
 	let f_mobile = trim(req.body.mobile || '');
 	let f_address = trim(req.body.address || '');
+    let f_email = trim(req.body.f_email || '');
 	let f_period0 = trim(req.body.period0 || '');
 	let f_period1 = trim(req.body.period1 || '');
 	let f_advisor = trim(req.body.advisor || '');
@@ -529,16 +607,16 @@ app.post('/api/v1/card', function(req, res) {
 			maxcid = parseInt(maxcid) + 1;
 			maxcid = '' + maxcid;
 			console.log({maxuid, maxcid});
-			const s1 = "INSERT INTO Tr_member_CardStatus(UserID,卡号,卡状态,描述,操作人员,操作日期,开卡门店) " +
-					"VALUES(@uid,@cid,1,'已发卡投入使用',@operator,GETDATE(),'总部')";
+			const s1 = "INSERT INTO Tr_member_CardStatus(UserID,卡号,卡状态,描述,操作人员,操作日期,开卡门店,标记) " +
+					"VALUES(@uid,@cid,1,'已发卡投入使用',@operator,GETDATE(),'总部','Y')";
 			const s2 = "INSERT INTO Tr_member_Cardbaseinfo(UserID,卡号,姓名,性别,身份证号码,联系电话,通讯地址,会员期限类别,益生套餐,采购健老,首次采购价格," +
-					"有效期起始,有效期截止,签发日期,用户密码,健康顾问,发卡门店,会员状态,操作日期,操作人员,卡片开启,备注,使用分类,定制电话) " +
+					"有效期起始,有效期截止,签发日期,用户密码,健康顾问,发卡门店,会员状态,操作日期,操作人员,卡片开启,备注,使用分类,定制电话,标记,电子邮件) " +
 					"VALUES(@uid,@cid,@username,@sex,@idnum,@mobile,@address,'3',3,0,@price,@period0,@period1,GETDATE(),'888888',@advisor,'总部'," +
-					"'新卡使用',GETDATE(),@operator,'开启',@comment,'新会员',@altphone)";
-			const s3 = "INSERT INTO Tr_member_Moneydetail(UserID,卡号,收款类型,项目名称,收款金额,收款门店,收款日期,收款人员,姓名,性别,身份证号码) " +
-					"VALUES(@uid,@cid,'首次入会','益生套餐',@price,'总部',GETDATE(),@operator,@username,@sex,@idnum)";
-			const s4 = "INSERT INTO Tr_member_RegCardinfo(UserID,卡号,姓名,性别,身份证号码,联系电话,通讯地址,健康顾问,发卡门店,发卡日期,操作人员) " +
-					"VALUES(@uid,@cid,@username,@sex,@idnum,@mobile,@address,@advisor,'总部',GETDATE(),@operator)";
+					"'新卡使用',GETDATE(),@operator,'开启',@comment,'新会员',@altphone,'Y',@email)";
+			const s3 = "INSERT INTO Tr_member_Moneydetail(UserID,卡号,收款类型,项目名称,收款金额,收款门店,收款日期,收款人员,姓名,性别,身份证号码,标记) " +
+					"VALUES(@uid,@cid,'首次入会','益生套餐',@price,'总部',GETDATE(),@operator,@username,@sex,@idnum,'Y')";
+			const s4 = "INSERT INTO Tr_member_RegCardinfo(UserID,卡号,姓名,性别,身份证号码,联系电话,通讯地址,健康顾问,发卡门店,发卡日期,操作人员,标记) " +
+					"VALUES(@uid,@cid,@username,@sex,@idnum,@mobile,@address,@advisor,'总部',GETDATE(),@operator,'Y')";
 			const trans = pool80.transaction();
 			trans.begin(err => {
 				if (err) {
@@ -555,8 +633,8 @@ app.post('/api/v1/card', function(req, res) {
                         await trans.request().input('uid',maxuid).input('cid',maxcid).input('operator',f_operator).query(s1);
                         await trans.request().input('uid',maxuid).input('cid',maxcid).input('username',f_name).input('sex',f_sex)
 							.input('idnum',id).input('mobile',f_mobile).input('address',f_address).input('price',f_price / 3.0)
-							.input('period0',f_period0).input('period1',f_period1).input('advisor',f_advisor)
-							.input('operator',f_operator).input('comment',f_comment).input('altphone',f_altphone).query(s2);
+							.input('period0',f_period0).input('period1',f_period1).input('advisor',f_advisor).input('operator',f_operator)
+							.input('comment',f_comment).input('altphone',f_altphone).input('email',f_email).query(s2);
                         await trans.request().input('uid',maxuid).input('cid',maxcid).input('price',f_price)
 							.input('operator',f_operator).input('username',f_name).input('sex',f_sex).input('idnum',id).query(s3);
                         await trans.request().input('uid',maxuid).input('cid',maxcid).input('username',f_name).input('sex',f_sex)
